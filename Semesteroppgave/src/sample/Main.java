9a8410df68e818cca46bc3e46b37c8dafa16baa3
DaHenke <henkem212@gmail.com>
2020-03-31 22:05:07 +0200

    En test

--- Semesteroppgave/src/sample/Main.java
+++ Semesteroppgave/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("logIn.fxml"));
-        primaryStage.setTitle("Hello WorldTesten");
+        primaryStage.setTitle("Hello World test");
         primaryStage.setScene(new Scene(root, 715, 522));
         primaryStage.show();
     }
