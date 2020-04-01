2efe847e31713344b005202091a675d616054445
s345871 <s345871@oslomet.no>
2020-04-01 14:46:57 +0200

    En test

--- Semesteroppgave/src/calculator/priceList.java
+++ Semesteroppgave/src/calculator/priceList.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import javafx.beans.property.SimpleDoubleProperty;
+import sun.java2d.pipe.SpanShapeRenderer;
 
 public class priceList {
 
@@ -16,4 +17,14 @@ public class priceList {
     public static final SimpleDoubleProperty ASUS_GeForce_GT710_1GB = new SimpleDoubleProperty(533);
     public static final SimpleDoubleProperty Gigabyte_GeForce_GT_1030_2GB_Low_Profile = new SimpleDoubleProperty(1099);
     public static final SimpleDoubleProperty PNY_Quadro_P400_2GB_DVI = new SimpleDoubleProperty(1_889);
+    public static final SimpleDoubleProperty Gainward_GeForce_GTX_1650_SUPER_Pegasus_OC = new SimpleDoubleProperty(2_399);
+
+    //minne
+    public static final SimpleDoubleProperty HyperX_Fury_DDR3_1600MHz_4GB = new SimpleDoubleProperty(333);
+    public static final SimpleDoubleProperty Crucial_DDR4_2400MHz_8GB = new SimpleDoubleProperty(564);
+    public static final SimpleDoubleProperty Kingston_Value_DDR4_3400MHz_16GB = new SimpleDoubleProperty(929);
+    public static final SimpleDoubleProperty HyperX_Fury_DDR4_2666MHZ_32GB = new SimpleDoubleProperty(1_940);
+
+    //harddisk
+
 }
