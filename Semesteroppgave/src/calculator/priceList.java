fe4c5582d5981d0c41f2688644d3f5783ffd82a6
s345871 <s345871@oslomet.no>
2020-04-01 18:02:45 +0200

    En test

--- Semesteroppgave/src/calculator/priceList.java
+++ Semesteroppgave/src/calculator/priceList.java
@@ -1,7 +1,6 @@
 package calculator;
 
 import javafx.beans.property.SimpleDoubleProperty;
-import sun.java2d.pipe.SpanShapeRenderer;
 
 public class priceList {
 
@@ -23,8 +22,39 @@ public class priceList {
     public static final SimpleDoubleProperty HyperX_Fury_DDR3_1600MHz_4GB = new SimpleDoubleProperty(333);
     public static final SimpleDoubleProperty Crucial_DDR4_2400MHz_8GB = new SimpleDoubleProperty(564);
     public static final SimpleDoubleProperty Kingston_Value_DDR4_3400MHz_16GB = new SimpleDoubleProperty(929);
-    public static final SimpleDoubleProperty HyperX_Fury_DDR4_2666MHZ_32GB = new SimpleDoubleProperty(1_940);
+    public static final SimpleDoubleProperty HyperX_Fury_DDR4_2666MHZ_32GB = new SimpleDoubleProperty(1_949);
 
-    //harddisk
+    //harddisk laptop 2.5"
+    public static final SimpleDoubleProperty WD_Mobile_Black_1TB_Harddisk = new SimpleDoubleProperty(807);
+    public static final SimpleDoubleProperty Seagate_Firecuda_2TB = new SimpleDoubleProperty(1_289);
+    public static final SimpleDoubleProperty Seagate_Barracuda_4TB = new SimpleDoubleProperty(2_174);
 
+    //harddisk desktop 3.5"
+    public static final SimpleDoubleProperty Seagate_Barracuda_1TB = new SimpleDoubleProperty(530);
+    public static final SimpleDoubleProperty Seagate_Barracuda_3TB = new SimpleDoubleProperty(1_022);
+    public static final SimpleDoubleProperty Seagate_SkyHawk_6TB = new SimpleDoubleProperty(2_310);
+    public static final SimpleDoubleProperty Seagate_IronWolf_10TB = new SimpleDoubleProperty(4_486);
+
+    //ekstern harddisk
+    public static final SimpleDoubleProperty Seagate_Expansion_Portable_1TB = new SimpleDoubleProperty(689);
+    public static final SimpleDoubleProperty ASUS_FX_External_Hard_Drive_3TB = new SimpleDoubleProperty(1_317);
+    public static final SimpleDoubleProperty WD_My_Book_8TB = new SimpleDoubleProperty(2_245);
+
+    //mus
+    public static final SimpleDoubleProperty Logitech_M90_Mus = new SimpleDoubleProperty(109);
+    public static final SimpleDoubleProperty Microsoft_Mobile_Mouse_3600 = new SimpleDoubleProperty(349);
+    public static final SimpleDoubleProperty Surface_ARC_mus = new SimpleDoubleProperty(829);
+    public static final SimpleDoubleProperty Logitech_MX_Master3_Adv_trådløs = new SimpleDoubleProperty(1_299);
+
+    //tastatur
+    public static final SimpleDoubleProperty Microsoft_Wired_Keyboard_600 = new SimpleDoubleProperty(199);
+    public static final SimpleDoubleProperty Logitech_K400_Plus_Trådløs_Tastatur = new SimpleDoubleProperty(549);
+    public static final SimpleDoubleProperty Kanex_MultiSync_Premium_Slim_Keyboard = new SimpleDoubleProperty(949);
+    public static final SimpleDoubleProperty Logitech_ERGO_K860_Trådløs_Tastatur = new SimpleDoubleProperty(1_399);
+
+    //monitor
+    public static final SimpleDoubleProperty Acer_24_skjerm_KA242Y = new SimpleDoubleProperty(1_395);
+    public static final SimpleDoubleProperty Samsung_24_skjerm_S24E450F = new SimpleDoubleProperty(1_695);
+    public static final SimpleDoubleProperty Acer_27_skjerm_KA272 = new SimpleDoubleProperty(2_195);
+    public static final SimpleDoubleProperty Samsung_28_4K_LED_FreeSync_U28E570D = new SimpleDoubleProperty(3_290);
 }
